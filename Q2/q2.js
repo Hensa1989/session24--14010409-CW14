2078cf05303c76420fb5a8e33e9ee1e2b79f67a9
hengameh <hengameh.sattari@yahoo.com>
2022-06-30 15:41:03 +0430

    add files for q3

---
+++ Q2/q2.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+
+$("select").change(function(){
+    
+    let text =''
+    $("select option:selected").each (function(){
+        text += $(this).text()
+        
+    })
+    $("#para").html(text)
+}).trigger("change")
+})
